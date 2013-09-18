d193cc090b8eafe47112a3f3f62e0280c939b539
coredata <github@hell-labs.org>
2013-09-18 16:19:16 +0200

    Skeleton for OptionParser to use CLI switches.

---
+++ bin/cvs_to_nam.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+require 'optparse'
+require_relative '../lib/CVSToNam'
+
+options = {}
+
+option_parser = OptionParser.new do |opts|
+
+  exe_name = File.basename($PROGRAM_NAME)
+  opts.banner = "Usage: #{exe_name} [options] cvs_file_name"
+
+  opts.on('-n', '--without-nam', 'No video board files will be generated.') do
+    options[:nam] = false
+  end
+
+  opts.on('-a', '--without-ares', 'No Ares21 files will be generated.') do
+    options[:ares] = false
+  end
+
+  opts.on('-t', '--team', 'Process a CSV for a team competition.') do
+    options[:finals] = true
+  end
+end
+
+begin
+  option_parser.parse!
+  if ARGV.empty?
+    puts 'Error: no CVS file name supplied'
+    puts
+    puts option_parser.help
+    exit 1
+  else
+    filename = ARGV.last
+    puts "You requested processing for CSV file: #{filename}"
+  end
+end
