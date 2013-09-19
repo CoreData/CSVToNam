3e087d1dbfdb0d7c558540e8cb852ab763c3e261
coredata <github@hell-labs.org>
2013-09-20 01:35:30 +0200

    Skeleton file for processing modules.

---
+++ lib/CSVToNam/individual/individual.rb
@@ -0,0 +1,8 @@
+module CSVToNam
+  module Individual
+    # TODO implement
+    def self.was_called
+      puts "#{self} was called!"
+    end
+  end
+end
---
+++ lib/CSVToNam/team/team.rb
@@ -0,0 +1,7 @@
+module CSVToNam
+  module Team
+    def self.was_called
+      puts "#{self} was called!"
+    end
+  end
+end
