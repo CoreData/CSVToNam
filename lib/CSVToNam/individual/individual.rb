e26dedc66424a3c6a7674066a467e71247a73356
coredata <github@hell-labs.org>
2013-09-23 20:33:15 +0200

    Modified skeleton methods to return function name when called.

--- lib/CSVToNam/individual/individual.rb
+++ lib/CSVToNam/individual/individual.rb
@@ -2,13 +2,13 @@ module CSVToNam
   module Individual
     # TODO implement code to handle csv files for individual competitions
     def self.was_called
-      puts "#{self} was called!"
+      puts "#{self}.#{__method__} was called!"
     end
 
     # This method handles an individual final competition.
     # The CSV file for this setting misses the "categories" line (line 3).
     def self.final
-      puts "#{self} was called!"
+      puts "#{self}.#{__method__} was called!"
     end
   end
 end
--- lib/CSVToNam/team/team.rb
+++ lib/CSVToNam/team/team.rb
@@ -2,7 +2,7 @@ module CSVToNam
   module Team
     # TODO implement code to handle csv files for team competitions
     def self.was_called
-      puts "#{self} was called!"
+      "#{self}.#{self.__method__} was called."
     end
   end
 end
