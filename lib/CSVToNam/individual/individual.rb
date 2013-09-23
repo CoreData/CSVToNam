11ced1ebdd6f858e7baa0c6ed1801e91dc394424
coredata <github@hell-labs.org>
2013-09-23 13:17:18 +0200

    Inserted skeleton method for finals processing

--- lib/CSVToNam/individual/individual.rb
+++ lib/CSVToNam/individual/individual.rb
@@ -1,7 +1,13 @@
 module CSVToNam
   module Individual
-    # TODO implement
+    # TODO implement code to handle csv files for individual competitions
     def self.was_called
+      puts "#{self} was called!"
+    end
+
+    # This method handles an individual final competition.
+    # The CSV file for this setting misses the "categories" line (line 3).
+    def self.final
       puts "#{self} was called!"
     end
   end
