69a2f6df844fff5ecfb8c8960a836a04dd5583dd
coredata <github@hell-labs.org>
2013-09-23 21:09:33 +0200

    Corrected a typo in was_called method.

--- lib/CSVToNam/team/team.rb
+++ lib/CSVToNam/team/team.rb
@@ -2,7 +2,7 @@ module CSVToNam
   module Team
     # TODO implement code to handle csv files for team competitions
     def self.was_called
-      "#{self}.#{self.__method__} was called."
+      puts "#{self}.#{__method__} was called."
     end
   end
 end
