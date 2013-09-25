5a6ba21c1586fc02f1da8cbd9849a9531d3f2a9d
coredata <github@hell-labs.org>
2013-09-25 17:14:16 +0200

    Require statement modified to reflect change of FileWriter functionality from class to module.

--- lib/CSVToNam.rb
+++ lib/CSVToNam.rb
@@ -4,15 +4,17 @@ require 'time'
 require 'csv'
 require 'optparse'
 
-# LOCAL
+# Local
+# Classes
 require 'CSVToNam/version'
 require 'CSVToNam/event'
-require 'CSVToNam/file_writer'
 require 'CSVToNam/storage'
 require 'CSVToNam/swimmer'
 require 'CSVToNam/tournament'
 
+# Modules
 require 'CSVToNam/csv_reader/csv_reader'
+require 'CSVToNam/file_writer/file_writer'
 require 'CSVToNam/Team/team'
 require 'CSVToNam/Individual/individual'
 
